e47b7c0acd399236256b7cabbf90cff9e9b91605
Furn <Furnacefurn@gmail.com>
2026-09-26 12:46:52 +0200

    feat: update app_version in variables.tf to v0.3.2

--- infra/variables.tf
+++ infra/variables.tf
@@ -27,7 +27,7 @@ variable "app_version" {
     version. Never point this at a branch.
   EOT
   type        = string
-  default     = "v0.3.1"
+  default     = "v0.3.2"
 }
 
 variable "api_key_parameter_name" {
